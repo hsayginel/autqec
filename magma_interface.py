099120b4e3a46c5955db0a645d8262e8dff4d0db
hsayginel <hasan.sayginel.17@ucl.ac.uk>
2024-08-19 12:43:41 +0100

    qec_embed_code

--- magma_interface.py
+++ magma_interface.py
@@ -3,6 +3,7 @@ import subprocess
 import re
 import pickle
 import itertools
+from math import comb
 
 class qec_code_auts_from_magma:
     def __init__(self,n,k,d,H_symp):
@@ -378,58 +379,54 @@ class qec_code_auts_from_magma_with_intersection:
 class qec_embed_code:
     def __init__(self, H_symp, embedding = 'two_code_blocks'):
         self.H_symp = H_symp 
+        n = H_symp.shape[1]//2
+        self.n = n
         self.embedding = embedding
 
-    def gen_w2_embed_mat(self,n):
+    def gen_two_block_embed_mat(self):
+        nhalf = self.n//2
+        id_mat = np.eye(nhalf,dtype=int)
+        zeros_mat = np.zeros_like(id_mat,dtype=int)
+
+        block_col1 = np.hstack((id_mat,zeros_mat))
+        block_col2 = np.hstack((zeros_mat,id_mat))
+        block_col3 = np.hstack((id_mat,id_mat))
+
+        return np.vstack((np.vstack((block_col1,block_col2)),block_col3)).T
+
+    def gen_w2_embed_mat(self):
+        n = self.n
         combos = itertools.combinations(range(n), 2)
-        matrix = []
-        for combo in combos:
-            column = [0] * n  
-            for index in combo:
-                column[index] = 1  
-            matrix.append(column)
-        matrix = [list(row) for row in zip(*matrix)]
-        matrix = np.hstack((np.eye(n),np.matrix(matrix)))
-      
-        return np.matrix(matrix,dtype=int)
+        w2_embed_mat = np.zeros((comb(n,2),n),dtype=int)
+        for row, pair in enumerate(combos):
+            w2_embed_mat[row,pair[0]] = 1
+            w2_embed_mat[row,pair[1]] = 1
+        return np.vstack((np.eye(n,dtype=int),w2_embed_mat)).T
 
     def embed_mat(self):
         H_symp = self.H_symp
-        n = H_symp.shape[1]//2
-
+        n = self.n
         option = self.embedding
+        
+        HX_part = H_symp[:,:n]
+        HZ_part = H_symp[:,n:]
         if option == 'two_code_blocks':
-            id_mat = np.eye(n,dtype=int)
-            zeros_mat = np.zeros_like(id_mat,dtype=int)
-
-            block_col1 = np.vstack((id_mat,zeros_mat))
-            block_col2 = np.vstack((zeros_mat,id_mat))
-            block_col3 = np.vstack((id_mat,id_mat))
-
-            V_T = np.hstack((np.hstack((block_col1,block_col2)),block_col3))
-
-        elif option == "three_code_blocks":
-            NotImplementedError
-
+            V_T = self.gen_two_block_embed_mat()
         elif option == 'all_weight_2': 
-            NotImplementedError
-            # HX_part = H_symp[:,:n]
-            # HZ_part = H_symp[:,n:]
-            # V_T = self.gen_w2_embed_mat(5)
-            # M_T = V_T[:,n:]
-            # M = M_T.T
-            # A_V = np.hstack((HX_part,(HX_part@M_T)%2))
-            # ones = np.eye(M.shape[0])
-            # zeros = np.zeros((HZ_part.shape[0],ones.shape[1]))
-            # B_V = np.vstack((np.hstack((HZ_part,zeros)),np.hstack((M,ones))))
-            # assert A_V.shape[1] == B_V.shape[1]
-            # no_of_rows = B_V.shape[0] - A_V.shape[0] 
-            # A_V = np.vstack((A_V,np.zeros((no_of_rows,A_V.shape[1]))))
-
-        elif option == 'all_weight_3':
-            NotImplementedError
-
+            V_T = self.gen_w2_embed_mat()
         else:
             raise TypeError("Unknown option for embedding.")
+        M_T = V_T[:,n:]
+        M = M_T.T
+        A_V = np.hstack((HX_part,(HX_part@M_T)%2))
+        ones = np.eye(M.shape[0])
+        zeros = np.zeros((HZ_part.shape[0],ones.shape[1]))
+        B_V = np.vstack((np.hstack((HZ_part,zeros)),np.hstack((M,ones))))
+        assert A_V.shape[1] == B_V.shape[1]
+        no_of_rows = B_V.shape[0] - A_V.shape[0] 
+        A_V = np.vstack((A_V,np.zeros((no_of_rows,A_V.shape[1]))))
+        return np.array(np.hstack((A_V,B_V)),dtype=int)
+    
         
-        return V_T
+        
+       
