1f9008063900ed2ce7ddef8f94d0d127204d279a
hsayginel <hasan.sayginel.17@ucl.ac.uk>
2024-08-21 11:10:12 +0100

    XY dualities

---
+++ codetables/XY_dualities/logicals_gates.py
@@ -0,0 +1,38 @@
+from XY_dualities import *
+
+d = '?'
+n_min = 1
+n_max = 15
+with open("codetables/XY_dualities/logicals_errors.txt", "w") as file:
+    for n in range(n_min,n_max+1):
+        for k in range(n+1):
+            try: 
+                print("Processing code [[{},{}]]".format(n,k))
+                H_symp = np.load(f'codetables/parity_checks/H_symp_n{n}k{k}.npy')
+                auts_file = f'codetables/XY_dualities/auts_data/auts_n{n}k{k}d?.pkl'
+                with open(auts_file, 'rb') as f:
+                    code_auts_dict = pickle.load(f)
+                auts = code_auts_dict['auts']
+                gates = {}
+                gates['physical'] = []
+                gates['logical'] = []
+                symp_mats = []
+                for aut in auts:
+                    phys_act = physical_circ_of_XY_duality(H_symp,aut)        
+                    phys_circ = phys_act.circ_w_pauli_correction()
+                    gates['physical'].append(phys_circ)
+                    logical_act = logical_circ_of_XY_duality(H_symp,aut)   
+                    logical_circ, symplectic_mat = logical_act.circ()
+                    logical_circ = logical_act.circ_w_pauli_correction()
+                    gates['logical'].append(logical_circ)
+                    symp_mats.append(symplectic_mat)
+
+                with open(f'codetables/XY_dualities/logical_gates/gates_n{n}k{k}.pkl', 'wb') as f:
+                    pickle.dump(gates, f)
+                with open(f'codetables/XY_dualities/logical_gates/symp_mats_n{n}k{k}.pkl', 'wb') as f:
+                    pickle.dump(symp_mats, f)
+
+            except Exception as e:
+                error_message = f"Case [[{n,k}]] failed with error: {e}\n"
+                file.write(error_message)
+                file.flush() 
---
+++ tests/XY/test_XY_dualities copy.py
@@ -0,0 +1,29 @@
+from XY_dualities import *
+from utils.qec import stabs_to_H_symp
+
+n = 5
+k = 1 
+d = '?'
+H_symp = np.load(f'codetables/parity_checks/H_symp_n{n}k{k}.npy') 
+code = qec_code_XY_dualities_from_magma_with_intersection(n,k,d,H_symp)
+auts_data = code.run('./tests/XY/')
+auts = auts_data['auts']
+
+#######################################################################
+print(auts)
+phys_act = physical_circ_of_XY_duality(H_symp,auts[1])
+bits_image = phys_act.bits_image
+circ, symp_mat_phys = phys_act.circ()
+circ_pauli_corr = phys_act.circ_w_pauli_correction()
+print(symp_mat_phys)
+print(circ)
+print(circ_pauli_corr)
+
+#######################################################################
+log_act = logical_circ_of_XY_duality(H_symp,auts[1])
+log_act.print_phys_circ()
+log_act.print_physical_act()
+logical_circ = log_act.circ()
+logical_circ_pauli_corr = log_act.circ_w_pauli_correction()
+print(logical_circ)
+print(logical_circ_pauli_corr)
