0facdcaf86a14395c56920b5709db5f37ad5fc6c
hsayginel <hasan.sayginel.17@ucl.ac.uk>
2024-08-21 11:41:10 +0100

    XY dualities

--- tests/XY/test_XY_dualities copy.py
+++ tests/XY/test_XY_dualities copy.py
@@ -1,8 +1,8 @@
 from XY_dualities import *
 from utils.qec import stabs_to_H_symp
 
-n = 5
-k = 1 
+n = 10
+k = 2 
 d = '?'
 H_symp = np.load(f'codetables/parity_checks/H_symp_n{n}k{k}.npy') 
 code = qec_code_XY_dualities_from_magma_with_intersection(n,k,d,H_symp)
@@ -11,16 +11,16 @@ auts = auts_data['auts']
 
 #######################################################################
 print(auts)
-phys_act = physical_circ_of_XY_duality(H_symp,auts[1])
+phys_act = physical_circ_of_XY_duality(H_symp,auts[0])
 bits_image = phys_act.bits_image
 circ, symp_mat_phys = phys_act.circ()
-circ_pauli_corr = phys_act.circ_w_pauli_correction()
 print(symp_mat_phys)
 print(circ)
+circ_pauli_corr = phys_act.circ_w_pauli_correction()
 print(circ_pauli_corr)
 
 #######################################################################
-log_act = logical_circ_of_XY_duality(H_symp,auts[1])
+log_act = logical_circ_of_XY_duality(H_symp,auts[0])
 log_act.print_phys_circ()
 log_act.print_physical_act()
 logical_circ = log_act.circ()
