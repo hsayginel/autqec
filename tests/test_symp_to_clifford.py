d1fcba0bf8c385f51802d47e9de1d35daa5f4882
hsayginel <hasan.sayginel.17@ucl.ac.uk>
2024-08-23 13:37:45 +0100

    symp mat to clifford

---
+++ tests/test_symp_to_clifford.py
@@ -0,0 +1,33 @@
+from automorphisms import circ_from_symp_mat
+from utils.symplectic import *
+from utils.linalg import inv_mod2
+
+def random_symp_mats(h):
+    _,n = h.shape
+    X_part = h[:,:n//2].copy()
+    Z_part = h[:,n//2:].copy()
+    h_ZX = np.hstack((Z_part,X_part))
+    symp_mat = (np.eye(n,dtype=int) + h_ZX.T@h)%2
+    assert is_symplectic(symp_mat)
+    return symp_mat
+
+n = 20
+for _ in range(10000):
+    h = np.random.randint(0, 2, n).reshape((1,n))
+    S1 = random_symp_mats(h)
+    h = np.random.randint(0, 2, n).reshape((1,n))
+    S2 = random_symp_mats(h)
+    h = np.random.randint(0, 2, n).reshape((1,n))
+    S3 = random_symp_mats(h)
+    h = np.random.randint(0, 2, n).reshape((1,n))
+    S4 = random_symp_mats(h)
+    h = np.random.randint(0, 2, n).reshape((1,n))
+    S5 = random_symp_mats(h)
+    h = np.random.randint(0, 2, n).reshape((1,n))
+    S6 = random_symp_mats(h)
+
+    S_in = np.mod(S1 @ S2 @ S3 @ S4 @ S5 @ S6,2)
+
+    gates = circ_from_symp_mat(S_in).run()
+    S_out = symp_mat_prods(gates,n//2)
+    assert np.allclose(S_in,S_out)
--- tests/test_utils/test_symplectic.py
+++ tests/test_utils/test_symplectic.py
@@ -26,7 +26,7 @@ symp_mat_prods
 symp_prod
 is_symplectic
 
-from automorphisms import symplectic_mat_to_logical_circ
+from automorphisms import circ_from_symp_mat
 import numpy as np
 from utils.symplectic import *
 
@@ -36,7 +36,7 @@ from utils.symplectic import *
 #########################################################################################################
 k = 4
 M_in = (CZ_gate(1,2,k) @ CZ_gate(3,4,k) @ CZ_gate(1,3,k))%2
-gates = symplectic_mat_to_logical_circ(M_in).run()
+gates = circ_from_symp_mat(M_in).run()
 M_out = symp_mat_prods(gates,k)
 # print(gates)
 assert np.allclose(M_in,M_out) 
@@ -44,14 +44,14 @@ assert np.allclose(M_in,M_out)
 #########################################################################################################
 k = 2
 M_in = (S_gate(1,2)@Xsqrt_gate(1,2))%2
-gates = symplectic_mat_to_logical_circ(M_in).run()
+gates = circ_from_symp_mat(M_in).run()
 M_out = symp_mat_prods(gates,k)
 assert np.allclose(M_in,M_out)
 
 #########################################################################################################
 k = 2
 M_in = (Xsqrt_gate(1,2)@S_gate(1,2))%2
-gates = symplectic_mat_to_logical_circ(M_in).run()
+gates = circ_from_symp_mat(M_in).run()
 M_out = symp_mat_prods(gates,k)
 # print(gates)
 assert np.allclose(M_in,M_out)
@@ -59,7 +59,7 @@ assert np.allclose(M_in,M_out)
 #########################################################################################################
 k=4
 M_in = (CNOT_gate(1,2,k))%2
-gates = symplectic_mat_to_logical_circ(M_in).run()
+gates = circ_from_symp_mat(M_in).run()
 M_out = symp_mat_prods(gates,k)
 # print(gates)
 assert np.allclose(M_in,M_out)
@@ -68,7 +68,7 @@ assert np.allclose(M_in,M_out)
 k=2
 M_in = (CNOT_gate(1,2,k) @ Xsqrt_gate(1,k) @ S_gate(1,k) @ CZ_gate(1,2,k))%2
 assert is_symplectic(M_in)
-gates = symplectic_mat_to_logical_circ(M_in).run()
+gates = circ_from_symp_mat(M_in).run()
 # print(gates)
 M_out = symp_mat_prods(gates,k)
 assert np.allclose(M_in,M_out)
@@ -77,7 +77,7 @@ assert np.allclose(M_in,M_out)
 k=4
 M_in = (Xsqrt_gate(1,k) @ S_gate(1,k) @ CZ_gate(1,2,k))%2
 assert is_symplectic(M_in)
-gates = symplectic_mat_to_logical_circ(M_in).run()
+gates = circ_from_symp_mat(M_in).run()
 M_out = symp_mat_prods(gates,k)
 # print(gates)
 assert np.allclose(M_in,M_out)
@@ -86,7 +86,7 @@ assert np.allclose(M_in,M_out)
 k=4
 M_in = (H_gate(2,k) @ S_gate(2,k))%2
 assert is_symplectic(M_in)
-gates = symplectic_mat_to_logical_circ(M_in).run()
+gates = circ_from_symp_mat(M_in).run()
 M_out = symp_mat_prods(gates,k)
 assert np.allclose(M_in,M_out)
 
@@ -94,7 +94,7 @@ assert np.allclose(M_in,M_out)
 k = 3
 M_in = (SWAP_gate(1,3,3)@SWAP_gate(2,3,3))%2
 assert is_symplectic(M_in)
-gates = symplectic_mat_to_logical_circ(M_in).run()
+gates = circ_from_symp_mat(M_in).run()
 M_out = symp_mat_prods(gates,k)
 assert np.allclose(M_in,M_out)
 
@@ -102,7 +102,7 @@ assert np.allclose(M_in,M_out)
 k = 3
 M_in = (SWAP_gate(1,3,k)@CNOT_gate(1,3,k)@SWAP_gate(2,3,k)@H_gate(1,k))%2
 assert is_symplectic(M_in)
-gates = symplectic_mat_to_logical_circ(M_in).run()
+gates = circ_from_symp_mat(M_in).run()
 M_out = symp_mat_prods(gates,k)
 assert np.allclose(M_in,M_out)
 
@@ -110,7 +110,7 @@ assert np.allclose(M_in,M_out)
 k = 3
 M_in = (SWAP_gate(1,2,k)@SWAP_gate(2,3,k)@S_gate(1,k)@S_gate(2,k)@H_gate(2,k)@Xsqrt_gate(3,k))
 assert is_symplectic(M_in)
-gates = symplectic_mat_to_logical_circ(M_in).run()
+gates = circ_from_symp_mat(M_in).run()
 M_out = symp_mat_prods(gates,k)
 assert np.allclose(M_in,M_out)
 
@@ -118,14 +118,14 @@ assert np.allclose(M_in,M_out)
 k = 2
 M_in = (CNOT_gate(1,2,k)@CNOT_gate(2,1,k)@S_gate(1,k)@CZ_gate(1,2,k))%2
 assert is_symplectic(M_in)
-gates = symplectic_mat_to_logical_circ(M_in).run()
+gates = circ_from_symp_mat(M_in).run()
 M_out = symp_mat_prods(gates,k)
 assert np.allclose(M_in,M_out)
 
 #########################################################################################################
 M_in = np.array([[1,1,0,1],[1,0,1,1],[0,0,0,1],[0,0,1,1]])
 assert is_symplectic(M_in)
-gates = symplectic_mat_to_logical_circ(M_in).run()
+gates = circ_from_symp_mat(M_in).run()
 M_out = symp_mat_prods(gates,k)
 assert np.allclose(M_in,M_out)
 
@@ -134,7 +134,7 @@ omega = np.eye(2*k,dtype=int)
 omega[:,:k], omega[:,k:] = omega[:,k:].copy(), omega[:,:k].copy()
 M_in_inv = omega@M_in.T@omega
 assert is_symplectic(M_in_inv)
-gates = symplectic_mat_to_logical_circ(M_in_inv).run()
+gates = circ_from_symp_mat(M_in_inv).run()
 M_out = symp_mat_prods(gates[::-1],k)
 assert np.allclose(M_in,M_out)
 
@@ -142,7 +142,7 @@ assert np.allclose(M_in,M_out)
 k = 2
 M_in = (CNOT_gate(1,2,2)@CNOT_gate(2,1,2))%2
 assert is_symplectic(M_in)
-gates = symplectic_mat_to_logical_circ(M_in).run()
+gates = circ_from_symp_mat(M_in).run()
 M_out = symp_mat_prods(gates,k)
 assert np.allclose(M_in,M_out)
 
@@ -151,14 +151,14 @@ omega = np.eye(2,dtype=int)
 k=1
 omega[:,:k], omega[:,k:] = omega[:,k:].copy(), omega[:,:k].copy()
 M_in_inv = omega@M_in.T@omega
-gates = symplectic_mat_to_logical_circ(M_in).run()
+gates = circ_from_symp_mat(M_in).run()
 # print(gates)
 M_out = symp_mat_prods(gates,k)
 M_out_inv = symp_mat_prods(gates[::-1],k)
 assert np.allclose(M_in,M_out)
 assert np.allclose(M_in_inv,M_out_inv)
 
-gates_inv = symplectic_mat_to_logical_circ(M_in_inv).run()
+gates_inv = circ_from_symp_mat(M_in_inv).run()
 M_out_inv2 = symp_mat_prods(gates_inv,k)
 M_out2 = symp_mat_prods(gates_inv[::-1],k)
 assert np.allclose(M_in_inv,M_out_inv)
@@ -167,6 +167,6 @@ assert np.allclose(M_in,M_out2)
 #########################################################################################################
 k = 4
 M_in = (H_gate(1,k)@H_gate(3,k))%2
-gates = symplectic_mat_to_logical_circ(M_in).run()
+gates = circ_from_symp_mat(M_in).run()
 M_out = symp_mat_prods(gates,k)
 assert np.allclose(M_in,M_out)
