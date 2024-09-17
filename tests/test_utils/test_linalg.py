1b8a13605be8c8df2dd74959c6f5e9274ced02e7
hsayginel <hasan.sayginel.17@ucl.ac.uk>
2024-09-17 14:23:48 +0100

    test

--- tests/test_utils/test_linalg.py
+++ tests/test_utils/test_linalg.py
@@ -1,6 +1,5 @@
 from utils.linalg import *
 from utils.symplectic import *
-from automorphisms import symplectic_mat_to_logical_circ
 
 assert is_identity_matrix(np.eye(5))
 assert is_matrix_full_rank(np.array([[1,0],[0,1]])) == True
@@ -12,12 +11,3 @@ M = np.array([[0,0,0,1,1,1,1],
 M_rref, r, R, C = rref_mod2(M)
 print(M_rref)
 assert np.allclose(M_rref,R@M@C%2)
-
-# k = 2
-# cnot_circ_in = (CNOT_gate(1,2,k)@CNOT_gate(2,1,k))%2
-# GL_part = cnot_circ_in[:k,:k]
-
-# gates, _ = CNOT_circ_from_GL_mat(GL_part)
-# cnot_circ_out = symp_mat_prods(gates,k)
-
-# assert np.allclose(cnot_circ_in,cnot_circ_out) 
