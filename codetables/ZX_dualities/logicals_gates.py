53ae3af56abe1620a4b1c7ba16cc2a06d021bcb1
hsayginel <hasan.sayginel.17@ucl.ac.uk>
2024-09-25 15:22:50 +0100

    codetables ZX dualities

--- codetables/ZX_dualities/logicals_gates.py
+++ codetables/ZX_dualities/logicals_gates.py
@@ -1,11 +1,12 @@
 from ZX_dualities import *
+from automorphisms import *
 
 d = '?'
 n_min = 1
 n_max = 44
 with open("codetables/ZX_dualities/logicals_errors.txt", "w") as file:
     for n in range(n_min,n_max+1):
-        for k in range(n+1):
+        for k in range(1,n+1):
             try: 
                 print("Processing code [[{},{}]]".format(n,k))
                 H_symp = np.load(f'codetables/parity_checks/H_symp_n{n}k{k}.npy')
@@ -18,12 +19,12 @@ with open("codetables/ZX_dualities/logicals_errors.txt", "w") as file:
                 gates['logical'] = []
                 symp_mats = []
                 for aut in auts:
-                    phys_act = physical_circ_of_ZX_duality(H_symp,aut)        
-                    phys_circ = phys_act.circ_w_pauli_correction()
+                    phys_act = circ_from_ZX_duality(H_symp,aut)        
+                    phys_circ,_ = phys_act.circ()
                     gates['physical'].append(phys_circ)
-                    logical_act = logical_circ_of_ZX_duality(H_symp,aut)   
-                    logical_circ, symplectic_mat = logical_act.circ()
-                    logical_circ = logical_act.circ_w_pauli_correction()
+                    logical_act = logical_circ_and_pauli_correct(H_symp,phys_circ)   
+                    logical_circ, symplectic_mat = logical_act.run()
+                    logical_circ,_ = logical_act.run()
                     gates['logical'].append(logical_circ)
                     symp_mats.append(symplectic_mat)
 
