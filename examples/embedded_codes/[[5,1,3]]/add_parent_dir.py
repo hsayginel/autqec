2f0c929d967b14f2aafe14ae3568d932833a3518
hsayginel <hasan.sayginel.17@ucl.ac.uk>
2024-09-26 17:35:14 +0100

    add_parent_dir

--- examples/embedded_codes/[[5,1,3]]/add_parent_dir.py
+++ examples/embedded_codes/[[5,1,3]]/add_parent_dir.py
@@ -4,5 +4,5 @@ import sys
 import inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
-parentdir = f'{parentdir}/..'
+parentdir = f'{parentdir}/../..'
 sys.path.insert(0, parentdir) 
