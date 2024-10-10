4d24378715b4256ee1f885ed5cb89dfb95ed111c
hsayginel <hasan.sayginel.17@ucl.ac.uk>
2024-10-10 22:42:21 +0100

    move embedded code funcs to own .py

--- magma_interface.py
+++ magma_interface.py
@@ -2,8 +2,6 @@ from automorphisms import *
 import subprocess
 import re
 import pickle
-import itertools
-from math import comb
 
 class qec_code_auts_from_magma:
     def __init__(self,n,k,d,H_symp):
@@ -376,57 +374,3 @@ class qec_code_auts_from_magma_with_intersection:
         return aut_gens, aut_gens_text
     
     
-class qec_embed_code:
-    def __init__(self, H_symp, embedding = 'two_code_blocks'):
-        self.H_symp = H_symp 
-        n = H_symp.shape[1]//2
-        self.n = n
-        self.embedding = embedding
-
-    def gen_two_block_embed_mat(self):
-        nhalf = self.n//2
-        id_mat = np.eye(nhalf,dtype=int)
-        zeros_mat = np.zeros_like(id_mat,dtype=int)
-
-        block_col1 = np.hstack((id_mat,zeros_mat))
-        block_col2 = np.hstack((zeros_mat,id_mat))
-        block_col3 = np.hstack((id_mat,id_mat))
-
-        return np.vstack((np.vstack((block_col1,block_col2)),block_col3)).T
-
-    def gen_w2_embed_mat(self):
-        n = self.n
-        combos = itertools.combinations(range(n), 2)
-        w2_embed_mat = np.zeros((comb(n,2),n),dtype=int)
-        for row, pair in enumerate(combos):
-            w2_embed_mat[row,pair[0]] = 1
-            w2_embed_mat[row,pair[1]] = 1
-        return np.vstack((np.eye(n,dtype=int),w2_embed_mat)).T
-
-    def embed_mat(self):
-        H_symp = self.H_symp
-        n = self.n
-        option = self.embedding
-        
-        HX_part = H_symp[:,:n]
-        HZ_part = H_symp[:,n:]
-        if option == 'two_code_blocks':
-            V_T = self.gen_two_block_embed_mat()
-        elif option == 'all_weight_2': 
-            V_T = self.gen_w2_embed_mat()
-        else:
-            raise TypeError("Unknown option for embedding.")
-        M_T = V_T[:,n:]
-        M = M_T.T
-        A_V = np.hstack((HX_part,(HX_part@M_T)%2))
-        ones = np.eye(M.shape[0])
-        zeros = np.zeros((HZ_part.shape[0],ones.shape[1]))
-        B_V = np.vstack((np.hstack((HZ_part,zeros)),np.hstack((M,ones))))
-        assert A_V.shape[1] == B_V.shape[1]
-        no_of_rows = B_V.shape[0] - A_V.shape[0] 
-        A_V = np.vstack((A_V,np.zeros((no_of_rows,A_V.shape[1]))))
-        return np.array(np.hstack((A_V,B_V)),dtype=int)
-    
-        
-        
-       
